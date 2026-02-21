dd28133adffd5c9f7db0243ed2bef0ee86dcc78e
V <vadim003600@gmail.com>
2026-02-21 16:33:15 -0600

    PROJ-41 Added termination signal routine to orchestrator proces.

--- server/orchestrator/orchestrator.c
+++ server/orchestrator/orchestrator.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include <signal.h>
+
+volatile sig_atomic_t terminate = 0;
+
+void set_terminate(int sig)
+{
+    (void)sig; // silences the compiler warning  
+    terminate = 1;
+}
 
 int main(void)
 {
+    // Connect the signal to a handler routine to allow supervisor process gracefully terminate the orchestrator process 
+    if(signal(SIGUSR1, set_terminate) == SIG_ERR)
+    {
+        perror("signal");
+        return 1;
+    }
+    
+
     int counter = 0;
 
     for(;;)
     {
+        // Check if parent wants to terminate the process 
+        if(terminate)
+        {
+            // Later gracefully terminate this process 
+            break;
+        }
+
         sleep(2);
         printf("Tick: %d\n", counter++);
+        fflush(stdout);
     }
-
+    
     return 0;
 }
