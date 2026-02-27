62c89ca135cfb6a5309841b357dbee3c10118e92
Andrea Gomez <twe27@txstate.edu>
2026-02-27 03:06:25 -0600

    PROJ-40: Created new scene "test-item"
    Will be used to test collisions and functions for the parent class "Pickup"

--- crumbling-coffers/Scripts/Item_classes/Collectibles/test-item.gd
+++ crumbling-coffers/Scripts/Item_classes/Collectibles/test-item.gd
@@ -1,1 +1,8 @@
 extends Common
+
+var ab = ability # set ability "ab" status to false. This is NOT an ability item.
+
+func _on_body_entered(body: CharacterBody2D) -> void:
+	_on_item_collected()
+	abilityCheck(ab)
+	
