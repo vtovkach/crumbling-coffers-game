d39acd35b4d6a39e4b4626bac5d9cc9e4c92ac42
Andrea Gomez <twe27@txstate.edu>
2026-02-27 02:07:56 -0600

    PROJ-40: Subclasses rare and legendary items made.
    These are the same as the common_item (for now), just adjusted based on their score value.

---
+++ crumbling-coffers/Scripts/Item_classes/Collectibles/Legendary_item.gd
@@ -0,0 +1,10 @@
+extends PickupItems
+
+var rarity_type = "legendary"
+var item_type = "collectible"
+var legendary_score = 20		# default score. Can be changed for each subclass.
+
+func _ready() -> void:
+	_set_rarity(rarity_type)
+	_set_itemType(item_type)
+	_set_score(legendary_score)
---
+++ crumbling-coffers/Scripts/Item_classes/Collectibles/Rare_item.gd
@@ -0,0 +1,10 @@
+extends PickupItems
+
+var rarity_type = "rare"
+var item_type = "collectible"
+var rare_score = 5		# default score. Can be changed for each subclass.
+
+func _ready() -> void:
+	_set_rarity(rarity_type)
+	_set_itemType(item_type)
+	_set_score(rare_score)
