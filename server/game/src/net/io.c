d947fcc26aef81dd9a8aabc05ced9f27a5b8393b
V <vadim003600@gmail.com>
2026-02-27 23:29:58 -0600

    PROJ-32 Created a basic udp_read just for initial testing.
    It reads and copies received datagram from the kernel's queue into temporary buffer, and then prints the message.
    
    The function will be revised and changed later.

--- server/game/src/net/io.c
+++ server/game/src/net/io.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
-#include <sys/types.h>
+#include <stdint.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-ssize_t udp_read()
+#define UDP_DATAGRAM_SIZE 20
+
+ssize_t udp_read(int target_fd)
 {
+    uint8_t temp_buf[UDP_DATAGRAM_SIZE];  
 
-    return 0;
+    ssize_t bytes = recvfrom(target_fd, temp_buf, sizeof(temp_buf), 0, NULL, NULL);
+    if (bytes <= 0)
+        return bytes;
+
+    fwrite(temp_buf, 1, bytes, stdout);
+    fflush(stdout);
+
+    return bytes;
 }
-
-
 
 ssize_t udp_write()
 {
-    
+
     return 0;
 }
