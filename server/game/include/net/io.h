d175df8224d7abc45314c7fe2de4407d3450201a
V <vadim003600@gmail.com>
2026-02-27 23:01:40 -0600

    PROJ-32 Added headers files for io.c and udp_socket.c

---
+++ server/game/include/net/io.h
@@ -0,0 +1,8 @@
+#ifndef _IO_
+#define _IO_
+
+ssize_t udp_read();
+
+ssize_t udp_write();
+
+#endif
---
+++ server/game/include/net/udp_socket.h
@@ -0,0 +1,8 @@
+#ifndef _UDP_SOCKET_H
+#define _UDP_SOCKET_H
+
+#include <stdint.h>
+
+int make_udp_server_socket(uint16_t port);
+
+#endif
