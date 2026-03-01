a58c6a630df5e09a3db98cc6d05a33ecb7625067
V <vadim003600@gmail.com>
2026-02-28 23:21:05 -0600

    PROJ-75 Added game process creation to test game process.

--- server/orchestrator/src/queue/game_queue.c
+++ server/orchestrator/src/queue/game_queue.c
@@ -1,5 +1,8 @@
 #include <stdio.h>                          
-#include "orchestrator/state/client.h"      
+#include "orchestrator/state/client.h"
+#include <unistd.h>      
+#include <sys/types.h>
+
 #include "server-config.h"            
 
 int addClientToQueue(struct Client *client)
@@ -18,5 +21,41 @@ int addClientToQueue(struct Client *client)
     putchar('\n');
     fflush(stdout);
 
+    /*
+    // For now, only used for 'testing' 
+    // Spawning game process 
+    pid_t p_pid = getpid();
+    pid_t g_pid = fork();
+    if(g_pid < 0)
+    {
+        // Error happened 
+        perror("[addClientQueue] fork");
+    }
+
+    if(g_pid == 0)
+    {
+        char string_p_pid[32];
+        char string_pipe_fd[32];
+
+        snprintf(string_p_pid, sizeof(string_p_pid), "%d", p_pid);
+        snprintf(string_pipe_fd, sizeof(string_pipe_fd), "%d", 476);
+
+        char *argv[] = {
+            GAME_PROCESS,
+            string_p_pid,
+            UDP_GAME_PROCESS_PORT,
+            string_pipe_fd,
+            NULL
+        };
+
+        execv(GAME_PROCESS, argv);
+
+        // Runs only if execv fails 
+        perror("[addClientQueue] execv");
+        _exit(1);
+    }
+    */
+   
     return 0;
+    
 }
