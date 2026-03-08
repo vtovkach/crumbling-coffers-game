574001b1193b14a2641e2d07c62fe5fc46ddc03f
V <vadim003600@gmail.com>
2026-03-07 22:07:48 -0600

    PROJ-91 Removed logging routines from the util file.

--- server/common/include/util.h
+++ server/common/include/util.h
@@ -7,8 +7,4 @@ int redirect_stderr(const char *path);
 
 void getTime(char *dest, size_t buf_size);
 
-void log_error_fd(FILE *const log_file, const char *err_msg, int conn_fd, int errno_code);
-
-void log_error(FILE *const log_file, const char *err_msg, int errno_code);
-
 #endif
