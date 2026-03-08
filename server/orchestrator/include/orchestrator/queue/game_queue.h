53477f7a1ffbcd98ccef7883710661606981bb8f
V <vadim003600@gmail.com>
2026-03-07 22:38:39 -0600

    PROJ-91 Defined GameQueue structure and essential game queue functions.
    Add GameQueue structure and basic management functions
    
    Introduce GameQueue structure containing a queue and a maximum client capacity
    
    Implement function to create a GameQueue
    
    Implement function to destroy a GameQueue

--- server/orchestrator/include/orchestrator/queue/game_queue.h
+++ server/orchestrator/include/orchestrator/queue/game_queue.h
@@ -2,7 +2,18 @@
 #define _GAME_QUEUE_H
 
 #include "orchestrator/state/client.h"      
+#include "ds/ds_queue.h"
 
-int addClientToQueue(struct Client *client);
+struct GameQueue
+{
+    Queue *gameQueue;
+    size_t max_capacity;
+};
+
+int addClientToQueue(struct GameQueue *gq, struct Client *client);
+
+struct GameQueue *createGameQueue();
+
+void freeGameQueue(struct GameQueue *gq);
 
 #endif
