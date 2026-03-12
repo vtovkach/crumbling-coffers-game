0a5b4079e85cb1339574b86cbba0794c2ecccf4e
V <vadim003600@gmail.com>
2026-03-11 23:32:15 -0500

    PROJ-92 Updated TCP Segment Size and UDP Segment Size

--- server/common/include/server-config.h
+++ server/common/include/server-config.h
@@ -9,13 +9,13 @@
 #define MAX_GAME_QUEUE          128
 #define ORCH_MAX_EPOLL_EVENTS   512
 #define HASH_TABLE_SIZE         4096
-#define TCP_SEGMENT_SIZE        20
+#define TCP_SEGMENT_SIZE        200
 
 // UDP GAME PROCESS 
 #define UDP_GAME_PROCESS_PORT   "10001"
 #define GAME_PROCESS            "./bin/game/game"
 #define GM_MAX_EPOLL_EVENTS     64  
-#define UDP_DATAGRAM_SIZE       20
+#define UDP_DATAGRAM_SIZE       200
 
 // GAME INFO 
 #define PLAYERS_PER_MATCH 2
