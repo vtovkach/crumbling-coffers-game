418e21bc54fb49f47157300ebbea31e7695104de
Daniel Pelley <daniel.pelley@outlook.com>
2026-03-18 19:52:26 -0500

    PROJ-112: spawn area change
    enlarged the spawn are for items to spawn in.

--- crumbling-coffers/Scenes/AreaToSpawnItems/spawn_area.gd
+++ crumbling-coffers/Scenes/AreaToSpawnItems/spawn_area.gd
@@ -2,20 +2,20 @@ extends Area2D
 
 @export var spawn_padding: float = 8.0
 
-
 func _ready():
 	add_to_group("spawn_areas")
-	
-	
+
 func get_random_point() -> Vector2:
-	var shape = $CollisionShape2D.shape
-	
+	var collision_shape = $CollisionShape2D
+	var shape = collision_shape.shape
+
 	if shape is RectangleShape2D:
 		var extents = shape.size * 0.5
-		
+		var local_center = collision_shape.position
+
 		var x = randf_range(-extents.x + spawn_padding, extents.x - spawn_padding)
 		var y = randf_range(-extents.y + spawn_padding, extents.y - spawn_padding)
-		
-		return global_position + Vector2(x, y)
-	
+
+		return to_global(local_center + Vector2(x, y))
+
 	return global_position
