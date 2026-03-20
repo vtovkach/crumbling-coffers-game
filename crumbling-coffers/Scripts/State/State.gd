34b02f3451bd5a06d8af88146ea746e63dcb25d0
Nicholas Krustchinsky <efa26@txstate.edu>
2026-03-20 15:54:05 -0500

    PROJ-80: make State base class NOT abstract
    abstracts MUST be overridden leading to code bloat when using "do nothing" implementation

--- crumbling-coffers/Scripts/State/State.gd
+++ crumbling-coffers/Scripts/State/State.gd
@@ -1,11 +1,17 @@
-@abstract
 class_name State
 extends Node
 
 signal transitioned
 
 # Enter state, update state, exit state cycle
-@abstract func enter()
-@abstract func exit()
-@abstract func update(delta: float)
-@abstract func physics_update(delta: float)
+func enter() -> void:
+	pass
+
+func exit() -> void:
+	pass
+	
+func update(delta: float) -> void:
+	pass
+	
+func physics_update(delta: float) -> void:
+	pass
