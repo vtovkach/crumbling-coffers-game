203a608ca763b2cd781e19479cdd3fc2b23e69bf
Nicholas Krustchinsky <efa26@txstate.edu>
2026-03-21 00:08:28 -0500

    PROJ-80: prevent infinite recursion potentially caused by workaround
    this is a messy solution but I wouldn't have it any other way

--- crumbling-coffers/Scripts/State/StateMachine.gd
+++ crumbling-coffers/Scripts/State/StateMachine.gd
@@ -7,6 +7,7 @@ var states : Dictionary = {}
 
 # This is not a great design. I'm not sure of alternative
 var stored_delta : float = 0
+var transitioning : bool
 
 func _ready() -> void:
 	for child in get_children():
@@ -28,11 +29,14 @@ func _physics_process(delta: float) -> void:
 
 func on_child_transition(state, new_state_name) -> void:
 # Prep
-	if state != active_state:
+	if state != active_state or transitioning:
 		return
+	
+	transitioning = true
 	
 	var new_state = states.get(new_state_name)
 	if !new_state:
+		transitioning = false
 		return
 	
 # Change of state
@@ -43,3 +47,4 @@ func on_child_transition(state, new_state_name) -> void:
 	
 	# Not great design. Update incoming state immediately so that a gameplay frame isn't skipped
 	active_state.physics_update(stored_delta)
+	transitioning = false
