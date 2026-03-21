f4ca5e27339569c51532a67ca8450cad9ecc7fb6
V <vadim003600@gmail.com>
2026-03-21 04:22:10 -0500

    PROJ-4 Implemented separate upd manager

--- crumbling-coffers/Netcode/udp_manager.gd
+++ crumbling-coffers/Netcode/udp_manager.gd
@@ -1,11 +1,47 @@
-extends Node
+extends Object
 
+var udp: PacketPeerUDP
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func init(ip: String, port: int) -> bool:
+	if udp:
+		udp.close()
 
+	udp = PacketPeerUDP.new()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	var err := udp.bind(0)
+	if err != OK:
+		push_error("UDP init failed: %s" % err)
+		return false
+
+	udp.set_dest_address(ip, port)
+	return true
+
+func send(packet: PackedByteArray) -> bool:
+	if not udp:
+		return false
+
+	if packet.is_empty():
+		return false
+
+	var err := udp.put_packet(packet)
+	return err == OK
+
+func receive() -> PackedByteArray:
+	if not udp:
+		return PackedByteArray()
+
+	if udp.get_available_packet_count() <= 0:
+		return PackedByteArray()
+
+	var packet := udp.get_packet()
+	var err := udp.get_packet_error()
+
+	if err != OK:
+		return PackedByteArray()
+
+	return packet
+
+func close() -> void:
+	if udp:
+		udp.close()
+	udp = null
