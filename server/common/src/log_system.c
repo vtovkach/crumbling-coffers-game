568d27957a7402bbdc309c6d30ca1c525c45d381
V <vadim003600@gmail.com>
2026-03-22 05:20:28 -0500

    PROJ-129 Used mutex_lock and mutex_unlock to protect critical section

--- server/common/src/log_system.c
+++ server/common/src/log_system.c
@@ -13,16 +13,19 @@ void log_message(FILE *const log_file, const char *msg)
     char time[TIME_BUFFER_SIZE];
     getTime(time, TIME_BUFFER_SIZE);
 
+    pthread_mutex_lock(&logging_lock);
     fprintf(log_file, "%s %s\n", time, msg);
+    pthread_mutex_unlock(&logging_lock);
 }
 
-void log_net_data(FILE *const log_file, char *const buf, size_t buf_size)
+void log_net_data(FILE *const log_file, const char *buf, size_t buf_size)
 {
     char time[TIME_BUFFER_SIZE];
     getTime(time, TIME_BUFFER_SIZE);
 
-    fprintf(log_file, "%s Received Data: ", time);
+    pthread_mutex_lock(&logging_lock);
 
+    fprintf(log_file, "%s Received Data: ", time);
     for(size_t i = 0; i < buf_size; i++)
     {
         if(buf[i] == '\0') 
@@ -34,8 +37,7 @@ void log_net_data(FILE *const log_file, char *const buf, size_t buf_size)
     }
     fputc('\n', log_file);
 
-    // Remove fflush later can hurt performance if called often 
-    fflush(log_file);
+    pthread_mutex_unlock(&logging_lock);
 }
 
 
@@ -44,10 +46,14 @@ void log_error(FILE *const log_file, const char *msg, int errno_code)
     char time[TIME_BUFFER_SIZE];
     getTime(time, TIME_BUFFER_SIZE);
 
+    pthread_mutex_lock(&logging_lock);
+
     if(errno_code == 0)
         fprintf(log_file, "%s %s\n", time, msg);
     else
-        fprintf(log_file, "%s %s: %s\n", time, msg, strerror(errno_code));
+        fprintf(log_file, "%s %s: %s\n", time, msg, strerror_r(errno_code));
+
+    pthread_mutex_unlock(&logging_lock);
 }
 
 void log_error_fd(FILE *const log_file, const char *err_msg, int conn_fd, int errno_code)
@@ -55,11 +61,12 @@ void log_error_fd(FILE *const log_file, const char *err_msg, int conn_fd, int er
     char time[TIME_BUFFER_SIZE]; 
     getTime(time, TIME_BUFFER_SIZE);
 
+    pthread_mutex_lock(&logging_lock);
+
     if(errno_code == 0)
         fprintf(log_file, "%s %s fd (%d)\n", time, err_msg, conn_fd);
     else
-        fprintf(log_file, "%s %s fd (%d): %s\n", time, err_msg, conn_fd, strerror(errno_code));
+        fprintf(log_file, "%s %s fd (%d): %s\n", time, err_msg, conn_fd, strerror_r(errno_code));
 
-    // Remove fflush later can hurt performance if called often 
-    fflush(log_file);
+    pthread_mutex_unlock(&logging_lock);
 }
