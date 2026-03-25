57e051811bf4fd7dde7bacbdf828dcc2275ae340
Daniel Pelley <daniel.pelley@outlook.com>
2026-03-25 14:35:23 -0500

    PROJ-111: added the freeze affect to test dummy.

--- crumbling-coffers/Scenes/TestDummy/test_dummy.gd
+++ crumbling-coffers/Scenes/TestDummy/test_dummy.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const SPEED = 600.0
 const GRAVITY = 900.0
+const FRICTION = 300.0
 var direction = 1
 
 var is_frozen: bool = false
@@ -25,15 +26,26 @@ func _ready() -> void:
 		
 	
 func _physics_process(delta):
+	if is_frozen:
+		freeze_time_left -= delta
+		if freeze_time_left <= 0.0:
+			clear_freeze()
+
 	if not is_on_floor():
-		velocity.y += GRAVITY * delta
-	else:
-		velocity.y = 0
+	
+		if not is_on_floor():
+			velocity.y += GRAVITY * delta
+		else:
+			velocity.y = 0
+			
+	
+	if  not is_frozen:
+		if ray_cast_right.is_colliding():
+			direction = -1
+		elif ray_cast_left.is_colliding():
+			direction = 1
 
-	if ray_cast_right.is_colliding():
-		direction = -1
-	elif ray_cast_left.is_colliding():
-		direction = 1
-
-	velocity.x = direction * SPEED
+		velocity.x = direction * SPEED
+	else: 
+		velocity.x = move_toward(velocity.x, 0, FRICTION*delta)
 	move_and_slide()
