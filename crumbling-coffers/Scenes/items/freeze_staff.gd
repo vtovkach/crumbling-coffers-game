37f88fe4bf8293ea4df10906aeb94fdeb8d9eeee
Daniel Pelley <daniel.pelley@outlook.com>
2026-03-25 14:34:26 -0500

    PROJ:111- Changes to freeze staff

--- crumbling-coffers/Scenes/items/freeze_staff.gd
+++ crumbling-coffers/Scenes/items/freeze_staff.gd
@@ -1,12 +1,15 @@
 extends "res://Scripts/pickup_base.gd"
 
 
-@export var freeze_duration: float = 2.0
+@export var freeze_duration: float = 5.0
 
 func _on_body_entered(body: Node) -> void:
 	for target in get_tree().get_nodes_in_group("freezable"):
+# For Testing against yourself - commment out chunk below.
 		if target == body:
 			continue
+
+	
 
 		if target.has_method("apply_freeze"):
 			target.apply_freeze(freeze_duration)
