da03407ed006fc4a2f947a91e2c597d30810f2e5
V <vadim003600@gmail.com>
2026-03-25 22:45:10 -0500

    PROJ-77 Updated sleep timer and path to log file

--- server/game/src/main.c
+++ server/game/src/main.c
@@ -11,7 +11,7 @@
 #include "net/net_thread.h"
 #include "signals.h"
 
-#define LOG_PATH "../../log/game"
+#define LOG_PATH "../../../log/game"
 
 atomic_bool net_stop = false;
 atomic_bool game_stop = false;
@@ -175,7 +175,7 @@ int main(int argc, char *argv[])
         goto failure; 
     }
 
-    sleep(5);
+    sleep(21);
 
     atomic_store(&game_stop, true);
     atomic_store(&net_stop, true);
